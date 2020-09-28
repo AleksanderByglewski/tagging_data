f019dbdcc2d72b66e5370bb5c96858a40165f084
AleksanderByglewski <67806043+AleksanderByglewski@users.noreply.github.com>
2020-09-28 12:04:32 +0200

    Adding a professional looking barrel roll

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print("Git changing")
     print("The tags are in the following form")
-
+def run_tale():
+    print("Run tale")
+    print("doing a barrel roll")
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
+    run_tale()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
